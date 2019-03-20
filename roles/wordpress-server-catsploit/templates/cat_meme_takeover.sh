68a1baff7ba92b6611031560291bc0a4c750462d
Kevin Holmes <kev@gokev.com>
2019-03-20 16:13:20 -0400

    kitty kitty

--- roles/wordpress-server-catsploit/templates/cat_meme_takeover.sh
+++ roles/wordpress-server-catsploit/templates/cat_meme_takeover.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-/bin/cp -p {{ wpwebpath }}/wp-content/themes/twentyseventeen/assets/images/header_cats.jpg "{{ wpwebpath }}/wp-content/themes/twentyseventeen/assets/images/header.jpg"
-
 mysql -u root {{ wpdbname }} < /tmp/cat_meme_takeover.sql
 
-echo DONE!  Now reload the web page to see what the evil cat hacker clan did!
+echo
+echo
+echo '    ##############################'
+echo '     __  __ _____ _____        __	'
+echo '    |  \/  | ____/ _ \ \      / /	' 
+echo '    | |\/| |  _|| | | \ \ /\ / / 	'
+echo '    | |  | | |__| |_| |\ V  V /  	'
+echo '    |_|  |_|_____\___/  \_/\_/   	' 
+echo ' 	       	       	       	       	'
+echo '    ##############################'
+echo
+echo     DONE!  Now reload the web page
+echo         http://{{ myhostname }}
+echo to see what the evil cat hacker clan did!
 echo
 echo
 
